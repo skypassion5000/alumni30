2643e5a02264e5f6b10b7cb3fc0ac2de2d0644c1
hsiang hsu <skypassion5000@gmail.com>
2023-12-22 08:31:19 +0800

    update gas

--- all.js
+++ all.js
@@ -5,7 +5,7 @@ function sendData() {
   };
 
   // 發送 JSON 資料至 Google Apps Script
-  axios.post('https://script.google.com/macros/s/AKfycbx0Bg2xOC62txEsOElc9Vcntx9fV9b4XraGRfdWAieP1FrdGgX_uC04kUXW4xJWkFv5/exec', data)
+  axios.post('https://script.google.com/macros/s/AKfycbxzAAWt_K4sWg04B9f-3JYUB6peLXORcoUn7y6v9Ifo4Up15DyGurLL0Fn6V_KJEmO-Vg/exec', data)
     .then(function (response) {
       console.log('Data sent successfully');
       // 在此可以處理成功傳送後的動作

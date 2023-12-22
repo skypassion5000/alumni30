dcc9c4efe27f5a1657bc985f3cd8af17d145ef8c
hsiang hsu <skypassion5000@gmail.com>
2023-12-22 08:39:14 +0800

    update to jquery

--- all.js
+++ all.js
@@ -1,17 +1,21 @@
-function sendData() {
-  var data = {
-    "name": "John Doe",
-    "email": "johndoe@example.com"
-  };
-
-  // 發送 JSON 資料至 Google Apps Script
-  axios.post('https://script.google.com/macros/s/AKfycbxzAAWt_K4sWg04B9f-3JYUB6peLXORcoUn7y6v9Ifo4Up15DyGurLL0Fn6V_KJEmO-Vg/exec', data)
-    .then(function (response) {
-      console.log('Data sent successfully');
-      // 在此可以處理成功傳送後的動作
-    })
-    .catch(function (error) {
-      console.error('Error sending data:', error);
-      // 在此可以處理錯誤的情況
+$(document).ready(function() {
+  $('#data-form').submit(function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    
+    // AJAX 請求傳送資料至 Google Apps Script
+    $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycby5pBj7mw-am0P8QNQOY2LCyWMAJNH8jxngpDpMR7o-puMRnrR1VTdLAM1opsTNNw42Bw/exec', // 填入你的 Google Apps Script 網址
+      method: 'GET',
+      data: formData,
+      success: function(response) {
+        console.log('Data sent successfully');
+        // 在此可以處理成功傳送後的動作
+      },
+      error: function(error) {
+        console.error('Error sending data:', error);
+        // 在此可以處理錯誤的情況
+      }
     });
-}
+  });
+});
